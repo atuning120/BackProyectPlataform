1415a58e13e3bac93cf6cccd456fc58670c58cb1
Gilberto Muñoz Benjamín Ignacio <131167469+Benlox44@users.noreply.github.com>
2025-03-26 13:03:41 -0300

    Revert "fix"
    This reverts commit 5d7f70677dda6242789168fbb9a25da668a2e349.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
 app.use("/api/patients", require("./routes/patients"));
 
 // Server port
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
