5d7f70677dda6242789168fbb9a25da668a2e349
Gilberto Muñoz Benjamín Ignacio <131167469+Benlox44@users.noreply.github.com>
2025-03-26 12:14:48 -0300

    fix

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
 app.use("/api/patients", require("./routes/patients"));
 
 // Server port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
